bf9048d25b2cec16c48395e04ecd9d66794f3db8
Thomas Nield <thomasnield@live.com>
2019-03-07 19:17:20 -0600

    Update Launcher.kt

--- module_setup/src/main/kotlin/Launcher.kt
+++ module_setup/src/main/kotlin/Launcher.kt
@@ -1,9 +1,9 @@
 import io.reactivex.Observable
 
 fun main() {
-		println("Hello from project!");
-		moduleAfunction()
-		moduleBfunction()
+	println("Hello from project!");
+	moduleAfunction()
+	moduleBfunction()
 
 
 	Observable.just("Hello from RxJava dependency in ModuleA!")
